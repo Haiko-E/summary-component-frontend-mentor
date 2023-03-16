e3fd24cdb80123b71624b641eb267b5d3a3f267e
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2023-03-16 20:54:59 +0100

    change sematics html

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const items = document.querySelector('.summary-items');
 
 data.forEach((item, i) => {
   // add wrapper
-  const itemDiv = items.appendChild(document.createElement('div'));
-  itemDiv.className = `summary-item ${colors[i]}`;
+  const li = items.appendChild(document.createElement('li'));
+  li.className = `summary-item ${colors[i]}`;
 
   //add img
-  const image = itemDiv.appendChild(document.createElement('img'));
+  const image = li.appendChild(document.createElement('img'));
   image.setAttribute('src', item.icon);
   image.setAttribute('alt', item.category);
 
   //add p
-  const p = itemDiv.appendChild(document.createElement('p'));
+  const p = li.appendChild(document.createElement('p'));
   p.textContent = item.category;
 
   //add span & bold
-  const span = itemDiv.appendChild(document.createElement('span'));
+  const span = li.appendChild(document.createElement('span'));
   const bold = document.createElement('b');
   bold.textContent = item.score;
   span.textContent = ' / 100';
